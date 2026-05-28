42bba4a6038ddd91dd260bea30479282c69d508a
Fern.N <vietlinhqv@gmail.com>
2026-05-28 17:50:10 +0700

    chore: remove sensitive credentials and temporary test files

--- apps/web/test-sentry.js
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-Sentry.init({
-  dsn: "https://c567210b733f3fc20ee9a5366bf0fbe7@o4511392076660736.ingest.us.sentry.io/4511392078954496",
-  enableLogs: true,
-});
-Sentry.captureException(new Error("Test Error from Antigravity"));
-console.log("Sent test error");
